276da729d0a9a39a786b3aceca282f863e578fff
Vineet Mishra <arvindmishra0993@gmail.com>
2026-04-03 18:58:27 +0530

    Project structure

--- methane-frontend/src/App.tsx
+++ methane-frontend/src/App.tsx
@@ -1,1 +1,23 @@
 //Frontend page:-
+// methane-frontend/
+// │
+// ├── public/
+// │
+// ├── src/
+// │   ├── components/
+// │   ├── data/
+// │   ├── hooks/
+// │   ├── lib/
+// │   ├── pages/
+// │   ├── App.tsx
+// │   ├── main.tsx
+// │   └── index.css
+// │
+// ├── .gitignore
+// ├── index.html
+// ├── package.json
+// ├── package-lock.json
+// ├── tsconfig.json
+// ├── tsconfig.app.json
+// ├── tsconfig.node.json
+// └── vite.config.ts
